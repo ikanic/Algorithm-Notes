a9a57a22930bdd2c4af0b553725f2eee2ee2e348
taek0622 <min4644@gmail.com>
2026-05-22 20:34:34 +0900

    wip

--- Swift/App/Sources/main.swift
+++ Swift/App/Sources/main.swift
@@ -56,6 +56,7 @@ public struct PGM118669: Solvable {
 
             // start end time -> time 정렬
             // start - end 순으로 pathTime 최적화
+            // 경로 만드는 것 고려
         }
 
         return []
