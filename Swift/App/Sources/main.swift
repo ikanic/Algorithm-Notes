f8b3b05df733168ce9f01831651b984893ac89b7
ikanic <min4644@gmail.com>
2026-05-26 21:28:39 +0900

    wip

--- Swift/App/Sources/main.swift
+++ Swift/App/Sources/main.swift
@@ -59,6 +59,7 @@ public struct PGM118669: Solvable {
             // 경로 만드는 것 고려
             // start - mid - end? mid 넣어야함?
             // mid 넣으면 현재 방법 안됨 => 새 방법 고려 필요
+            // start - end로 경로 확인하면서 갱신
         }
 
         return []
