6a035c6904c88a640f61e939e1c8efa8c9fa9874
ikanic <min4644@gmail.com>
2026-05-30 20:45:20 +0900

    wip

--- Swift/App/Sources/main.swift
+++ Swift/App/Sources/main.swift
@@ -57,8 +57,7 @@ public struct PGM118669: Solvable {
 
             // start end time -> time 정렬
             // start - end 순으로 pathTime 최적화
-            // 경로 만드는 것 고려
-            // start - mid - end? mid 넣어야함?
+            // start - mid - end
             // mid 넣으면 현재 방법 안됨 => 새 방법 고려 필요
             // start - end로 경로 확인하면서 갱신
         }
