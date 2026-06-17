03e81147548118a952ce2dfbb471ca7c3cf39aad
ikanic <min4644@gmail.com>
2026-06-17 21:35:17 +0900

    wip

--- Swift/App/Sources/main.swift
+++ Swift/App/Sources/main.swift
@@ -55,7 +55,6 @@ public struct PGM118669: Solvable {
             let now = heap.pop()!
             pathTime[now.start][now.end] = min(pathTime[now.start][now.end], now.time)
             // 현재는 start - mid 혹은 mid - end만 넣고 있음
-            // start-mid-end로 이어서 최적화할 방법 고려필요
             // start - mid는 그대로 두고 mid - end 위주로 갱신
             // 갱신시 이전 mid - end 값도 참고해서 이번 mid - end 비교에도 사용
 
